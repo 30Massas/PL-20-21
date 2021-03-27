b632be68780be2a0afb70028d4c9f24d22327e8a
56Aang <a89588@alunos.uminho.pt>
2021-03-27 16:51:11 +0000

    cleaning up the code

--- TP1/query3.py
+++ TP1/query3.py
@@ -6,7 +6,6 @@ def Query3():
     processos_avaliados = set()
 
     """
-
 {
     tio paterno:4
     tio materno:6
@@ -15,16 +14,19 @@ def Query3():
     primo
     
 }
-
     """
     graus = {}
 
-
     f=open('processos.xml','r')
     processos = gf.getProcessos(f.read())
+
+
     for p in processos:
+
         pr = p[0]
+
         if _id_ := gf.getId(pr):
+
             if _id_ in processos_avaliados:
                 pass
             else:
@@ -32,17 +34,16 @@ def Query3():
             
 
                 if m:= re.search(r'<obs>((.|\n)+?)</obs>',pr):
+                    
                     l_partida = m.group(1).split(".")
-                    
-                    #print(l_partida)
-                    
+
                     for p in l_partida:
                         p = p.strip()
                         p = re.sub(r'\s+',r' ',p)
                         a = p.split(",")
-                        #print(a)
+
                         if len(a) >= 2: # a[-1] -> parentesco
-                            #print(a)
+
                             if m := re.match(r'(?i:((tio|irmao|primo)(s)? ?(avo)?) ?(materno|paterno)?\3?)',a[-1]):
                                 # 0 -> tio|irmao|primo + avo + materno|paterno
                                 # 1 -> tio|irmao|primo + avo plural -> não usar
@@ -54,17 +55,15 @@ def Query3():
 
                                 if m.group(3):
                                     # parse no a[-2] "joao e andré"
+
                                     x = re.split(r' e | e',a[-2])
-                                    #print(a)
                                     aux = a[-1]
                                     a.remove(a[-2])
                                     a.remove(a[-1])
                                     for item in x:
                                         a.append(item)
                                     a.append(aux)
-                                    #print(a)
-                                    #a.append(aux)
-                                    #print(a)
+
                                 if m.group(4) and m.group(5):
                                     _key_ = m.group(2).strip() + " " + m.group(4).strip() + " " + m.group(5).strip()
                                     #print(_key_)
@@ -84,6 +83,8 @@ def Query3():
                                     graus.update({_key_: len(a)-1})
 
     graus = dict(sorted(graus.items(), key=lambda p:p[1], reverse=True))
+
     for grau in graus.keys():
         print(f'{grau} :> {graus.get(grau)}')
+    
     print(f'\nO grau de parentesco mais frequente é {list(graus.keys())[0]}')
