702f873f8adebc781a9b5f22edd4988f6cd166e5
56Aang <a89588@alunos.uminho.pt>
2021-03-29 23:09:54 +0100

    fix q5

--- TP1/generalFunctions.py
+++ TP1/generalFunctions.py
@@ -10,9 +10,25 @@ def getSeculo(ano):
         cent = int(i/100 + 1)
     return cent
 
+def getYear(process):
+    if year := re.search(r'<data>(\d{4})-\d{2}-\d{2}</data>',process):
+        return year.group(1)
+
 def getProcessos(lines):
     m = re.findall(r'(<processo .*>((.|\n)+?)</processo>)',lines)
     return m
+
+def getProcessosByData(lines, date):
+    proc = getProcessos(lines)
+    l = []
+    for p in proc:
+        pr = p[0]
+        y = getYear(pr)
+
+        if y == date:
+
+            l.append(pr)
+    return l
 
 def getId(line):
     if m:=re.search(r'<processo id="(\d+)">',line):
--- TP1/query5.py
+++ TP1/query5.py
@@ -1,21 +1,73 @@
 import re
 import generalFunctions as gf
+from itertools import tee # iterators
 from graphviz import Digraph
 
+ 
+dot = Digraph(comment="teste")
 
+processos_avaliados = set()
+f = open('teste.xml', 'r')
+processos = None
 
+def adicionaPais(name,temp,ano, filho):
+    
+    for pr in processos:
+        if _id_ := gf.getId(pr):
+            if _id_ not in temp:
+                if me := re.search(r'<nome>((.|\n)*)</nome>',pr):
+                    me = me.group(1)
+                    if me == name: # nome igual
+                        temp.add(_id_)
+                        
+                        if _id_ in processos_avaliados:
+                            dot.edge(filho,_id_)
+                        else:
+                            processos_avaliados.add(_id_)
+                            dot.node(_id_,name)
+                            dot.edge(filho,_id_)
+                            
+                            if mae := re.search(r'<mae>((.|\n)*)</mae>',pr):
+                                mae = mae.group(1)
+                                adicionaPais(mae,temp,ano, _id_)
+                            if pai := re.search(r'<pai>((.|\n)*)</pai>',pr):
+                                pai = pai.group(1)
+                                adicionaPais(pai,temp,ano, _id_)
+                        return
+                    
 def Query5():
     
-    f = open('processos.xml', 'r')
-
-    processos = gf.getProcessos(f.read())
-
-    dot = Digraph(comment="teste")
-
+    
     ano = input('Indique o ano que pretende investigar: ')
+    
+    global processos
+    processos = gf.getProcessosByData(f.read(), ano)
+    
+    for pr in processos:
+        if _id_ := gf.getId(pr):
+            if _id_ in processos_avaliados:
+                pass
+            else:
 
-    processos_avaliados = set()
-    idBD = 0
+                if filho := re.search(r'<nome>((.|\n)*)</nome>',pr):
+                    processos_avaliados.add(_id_)
+                    filho = filho.group(1)
+                    dot.node(_id_,filho)
+                    
+                    if mae := re.search(r'<mae>((.|\n)*)</mae>',pr):
+                        mae = mae.group(1)
+                        temp = set()
+                        temp.add(_id_)
+                        adicionaPais(mae,temp,ano, _id_)
+                    if pai := re.search(r'<pai>((.|\n)*)</pai>',pr):
+                        pai = pai.group(1)
+                        temp = set()
+                        temp.add(_id_)
+                        adicionaPais(pai,temp,ano, _id_)
+    # idBD = 0
+    
+    
+    """
     for p in processos:
         pr = p[0]
 
@@ -39,19 +91,30 @@ def Query5():
                                 mae = mae.group(1)
                             
                             
-                            dot.node(filho+str(idBD),filho)
-                            if pai:
-                                dot.node(pai+str(idBD+1),pai)
-                                dot.edge(filho+str(idBD),pai+str(idBD+1))
-                            if mae: 
-                                dot.node(mae+str(idBD+1),mae)
-                                dot.edge(filho+str(idBD),mae+str(idBD+1))
+                            if pai and mae:
+                                dot.node(filho+pai+mae,filho)
+                                dot.node(pai+mae,pai)
+                                dot.edge(filho+pai+mae,pai+mae)
+                        
+                                dot.node(mae+pai,mae+pai)
+                                dot.edge(filho+pai+mae,mae+pai)
+                            elif pai:
+                                dot.node(filho+pai,filho)
+                                dot.node(pai,pai)
+                                dot.edge(filho+pai,pai)
+                            elif mae:
+                                dot.node(filho+mae,filho)
+                                dot.node(mae,mae)
+                                dot.edge(filho+mae,mae)
+                        
+                        
                             idBD+=1
-                            
-                                               
+                   """         
+                                             
     g = open('test.gv','r+')
     g.write(dot.source)
     g.close()
     
     dot.render('test.gv',view=True)
+
     f.close()
